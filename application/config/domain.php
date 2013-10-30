082467ead15325b6aa9e195260d10bd08ca63068
komi2013 <komatsuka@yahoo.com>
2013-10-30 10:58:36 +0900

    force add domain.php

---
+++ application/config/domain.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+return array(
+  'dir' => '/stg',
+
+  'fb_id' => 'client_id=667157173308772&',
+  'fb_secret' => 'client_secret=c7d12d3e5cec263f086765670c777573&',
+
+  'tw_id' => 'Tcd6JT9Wa3b3BJDdvooVg',
+  'tw_secret' => 'GVyOtqnhifwyMvrgdMekooWaQdeBLvCKSA9AwfKyMLk',
+  'tw_callbak' => 'https://review-stg.komahana.info/usr/twcallback3/',
+  'test' => ' 10 ',
+);
