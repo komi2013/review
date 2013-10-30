277a7340302b2cdb1ff0bca3c9c994910838dbde
komi2013 <komatsuka@yahoo.com>
2013-10-30 19:05:24 +0900

    dir test

---
+++ application/classes/controller/test/dir.php
@@ -0,0 +1,23 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+class Controller_Test_Dir extends Controller_Template {
+
+  public function action_index()
+  {
+    echo APPPATH;
+//     $this->template = View::factory('/home/test');
+//     $u_id = isset($_GET['u_id'])? $_GET['u_id'] : 'ありません' ;
+//     $enqu = isset($_GET['enqu'])? $_GET['enqu'] : 'ありません' ;
+//     $passwd = isset($_GET['passwd'])? $_GET['passwd'] : 'ありません' ;
+//     $res['msg'] = '回答ありがとうございました。'
+//       ."<br>".'u_idは'.$u_id
+//       ."<br>".'enquは'.$enqu
+//       ."<br>".'passwdは'.$passwd
+//       ;
+//       
+//       
+//              
+//     die(json_encode($res));
+	}
+
+}
+//end
