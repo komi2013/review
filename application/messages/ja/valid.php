2b00ba2e5df6420cf84a6ef0952c12858777be8f
komi2013 <komatsuka@yahoo.com>
2013-11-06 12:23:46 +0900

    change msg

--- application/messages/ja/valid.php
+++ application/messages/ja/valid.php
@@ -4,5 +4,5 @@ return array(
 	'digit'         => ':field を数字にしてください',
 	'min_length'    => ':field で :param2 文字以下はダメです',
 	'max_length'    => ':field で :param2 文字以上はダメです',
-	'not_empty'     => ':field 空はダfffffメです',
+	'not_empty'     => ':field 空はダメです',
 );
